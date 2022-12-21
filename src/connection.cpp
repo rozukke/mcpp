e67c87f403003abf8b4b913a06a3f259eeaf1086
rozukke <yuutsunoyatsu@gmail.com>
2022-12-21 22:03:35 +1100

    Change socket system to use Boost Asio

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,47 +1,41 @@
 #include <string>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdexcept>
 #include "connection.h"
+#include <boost/asio.hpp>
+#include <iostream>
 
-#define MAX_PACKET_SIZE 4096
-
+using boost::asio::ip::tcp;
+using namespace boost;
 using std::string, std::string_view;
 
-SocketConnection::SocketConnection(string_view address_str, int port) {
-    //init socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == INVALID_SOCKET) {
-        throw std::runtime_error("Error creating socket");
+
+namespace mcpp {
+    SocketConnection::SocketConnection(string_view address_str, uint16_t port) {
+
+        asio::io_context io_context;
+        this->socket = new tcp::socket(io_context);
+        tcp::resolver resolver(io_context);
+        asio::connect(*socket, resolver.resolve(address_str, std::to_string(port)));
     }
 
-
-    //parse arg addresses
-    if (address_str == "localhost") {
-        address_str = "127.0.0.1";
+    SocketConnection &SocketConnection::operator=(const SocketConnection &other) {
+        return *this;
     }
-    inet_aton(address_str.data(), &addr.sin_addr);
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
 
-    conn();
-}
-
-void SocketConnection::conn() {
-    const int result = ::connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));
-    if (result == -1) {
-        throw std::runtime_error("Connection Failed");
+    void SocketConnection::send(string_view data_str) const {
+        socket->write_some(asio::buffer(data_str));
+//        asio::write(*socket, asio::buffer(data_str));
     }
-}
 
-void SocketConnection::send(string_view data_str) const {
-    ::send(sockfd, data_str.data(), data_str.length(), 0);
-}
+    string SocketConnection::recv() const {
+//        std::cout << "Receiving message: ";
+        std::string response;
+        asio::read_until(*socket, asio::dynamic_buffer(response), '\n');
+//        std::cout << response.data() << std::endl;
 
-string SocketConnection::recv() const {
-    char msg[MAX_PACKET_SIZE];
-    const size_t bytes_received = ::recv(sockfd, msg, MAX_PACKET_SIZE, 0);
-//    std::cout << (string)msg << "of size" << bytes_received <<std::endl;
-    return (string) msg;
+        //remove trailing \n
+        response.pop_back();
+
+        return response;
+    }
+
 }
--- src/connection.h
+++ src/connection.h
@@ -1,21 +1,65 @@
 #pragma once
+
 #include <string>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <iostream>
 #include <arpa/inet.h>
 #include <stdexcept>
+#include <initializer_list>
+#include <memory>
+#include <utility>
+#include <boost/asio.hpp>
 
-class SocketConnection {
-public:
-    explicit SocketConnection(std::string_view address_str = "localhost", int port = 4711);
-    void send(std::string_view data_str) const;
-    std::string recv() const;
+namespace mcpp {
 
-private:
-    const int INVALID_SOCKET = -1;
-    struct sockaddr_in addr{};
-    std::string response;
-    int sockfd;
-    void conn();
-};
+    class SocketConnection {
+    private:
+        boost::asio::ip::tcp::socket *socket;
+        const std::string FailedCommandResponse = "Fail";
+
+    public:
+        SocketConnection(std::string_view address_str = "localhost", uint16_t port = 4711);
+
+        void send(std::string_view data_str) const;
+
+        [[nodiscard]] std::string recv() const;
+
+        template<typename... Types>
+        void sendCommand(const std::string& prefix, Types ...args) {
+            std::string stringBuild;
+
+            stringBuild.append(prefix);
+            stringBuild.append("(");
+
+            // iterate over args pack
+            ((stringBuild.append(handleArg(args))), ...);
+
+            stringBuild.pop_back();
+            stringBuild.append(")\n");
+
+//            std::cout << stringBuild << std::endl;
+            send(stringBuild);
+        }
+
+        template<typename T, typename... Types>
+        std::string sendReceiveCommand(T prefix, Types ...args) {
+            sendCommand(prefix, args...);
+            auto result = recv();
+            return result;
+        }
+
+        SocketConnection &operator=(const SocketConnection &other);
+
+    private:
+
+        // Used within sendCommand to handle different types provided to it
+        template<typename T>
+        std::string handleArg(T arg) {
+            std::stringstream ss;
+            ss << arg << ",";
+            return ss.str();
+        }
+
+    };
+}
