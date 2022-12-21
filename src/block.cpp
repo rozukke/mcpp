e31980047d1b5a9d3db5293f42cc7d94520f36a2
rozukke <yuutsunoyatsu@gmail.com>
2022-12-21 22:04:58 +1100

    Block changes and block palette

--- src/block.cpp
+++ src/block.cpp
@@ -1,1 +1,10 @@
+#include "block.h"
 
+//this compares the data as well!!
+bool BlockType::operator==(const BlockType &other) const {
+    return this->id == other.id && this->data == other.data;
+}
+
+BlockType BlockType::withData(int newData) const {
+    return BlockType(this->id, newData);
+}
