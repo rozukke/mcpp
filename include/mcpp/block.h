e59d2d46c1e30052c10aae33f35f92628d833d9d
Artemis <yuutsunoyatsu@gmail.com>
2023-05-20 13:50:06 +1000

    Rearrange doc comments for block.h

--- include/mcpp/block.h
+++ include/mcpp/block.h
@@ -7,8 +7,18 @@ public:
 public:
     constexpr BlockType(int id, int data = 0) : id(id), data(data) {};
 
+    /**
+    * Watch out as this also compares the BlockType.id element of the block, so some equalities may behave in
+    * somewhat unexpected ways e.g. rotated stairs
+    */
     bool operator==(const BlockType &other) const;
 
+    /**
+    * Returns a new BlockType object with the specified data. Useful when providing a BlockType to a
+    * function that accepts them to quickly modify.
+    * @param newData Integer representing the data for the BlockType
+    * @return New BlockType object with the specified data
+    */
     [[nodiscard]] BlockType withData(int data) const;
 };
 
@@ -202,8 +212,10 @@ struct Blocks {
     static constexpr BlockType COBBLESTONE_MONSTER_EGG = BlockType(97, 1);
     static constexpr BlockType STONE_BRICK_MONSTER_EGG = BlockType(97, 2);
     static constexpr BlockType MOSSY_STONE_BRICK_MONSTER_EGG = BlockType(97, 3);
-    static constexpr BlockType CRACKED_STONE_BRICK_MONSTER_EGG = BlockType(97, 4);
-    static constexpr BlockType CHISELED_STONE_BRICK_MONSTER_EGG = BlockType(97, 5);
+    static constexpr BlockType CRACKED_STONE_BRICK_MONSTER_EGG = BlockType(97,
+                                                                           4);
+    static constexpr BlockType CHISELED_STONE_BRICK_MONSTER_EGG = BlockType(97,
+                                                                            5);
     static constexpr BlockType STONE_BRICKS = BlockType(98);
     static constexpr BlockType MOSSY_STONE_BRICKS = BlockType(98, 1);
     static constexpr BlockType CRACKED_STONE_BRICKS = BlockType(98, 2);
@@ -300,12 +312,14 @@ struct Blocks {
     static constexpr BlockType WHITE_STAINED_GLASS_PANE = BlockType(160);
     static constexpr BlockType ORANGE_STAINED_GLASS_PANE = BlockType(160, 1);
     static constexpr BlockType MAGENTA_STAINED_GLASS_PANE = BlockType(160, 2);
-    static constexpr BlockType LIGHT_BLUE_STAINED_GLASS_PANE = BlockType(160, 3);
+    static constexpr BlockType LIGHT_BLUE_STAINED_GLASS_PANE = BlockType(160,
+                                                                         3);
     static constexpr BlockType YELLOW_STAINED_GLASS_PANE = BlockType(160, 4);
     static constexpr BlockType LIME_STAINED_GLASS_PANE = BlockType(160, 5);
     static constexpr BlockType PINK_STAINED_GLASS_PANE = BlockType(160, 6);
     static constexpr BlockType GRAY_STAINED_GLASS_PANE = BlockType(160, 7);
-    static constexpr BlockType LIGHT_GRAY_STAINED_GLASS_PANE = BlockType(160, 8);
+    static constexpr BlockType LIGHT_GRAY_STAINED_GLASS_PANE = BlockType(160,
+                                                                         8);
     static constexpr BlockType CYAN_STAINED_GLASS_PANE = BlockType(160, 9);
     static constexpr BlockType PURPLE_STAINED_GLASS_PANE = BlockType(160, 10);
     static constexpr BlockType BLUE_STAINED_GLASS_PANE = BlockType(160, 11);
--- src/block.cpp
+++ src/block.cpp
@@ -1,19 +1,9 @@
 #include "../include/mcpp/block.h"
 
-/**
- * Watch out as this also compares the BlockType.id element of the block, so some equalities may behave in
- * somewhat unexpected ways e.g. rotated stairs
- */
 bool BlockType::operator==(const BlockType &other) const {
     return this->id == other.id && this->data == other.data;
 }
 
-/**
- * Returns a new BlockType object with the specified data. Useful when providing a BlockType to a
- * function that accepts them to quickly modify.
- * @param newData Integer representing the data for the BlockType
- * @return New BlockType object with the specified data
- */
 BlockType BlockType::withData(int newData) const {
-    return {this->id, newData};
+    return { this->id, newData };
 }
