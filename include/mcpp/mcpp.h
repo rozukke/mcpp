ff406782d6713fc603b4a32dfd0ee70cb9d4ebca
Johnathan Chan <jchan0867@gmail.com>
2024-06-23 20:31:59 +1000

    add/doxygen

--- include/mcpp/mcpp.h
+++ include/mcpp/mcpp.h
@@ -6,23 +6,35 @@
 #include <memory>
 #include <string_view>
 #include <vector>
+/** @file
+ * @brief MinecraftConnection class.
+ * 
+ */
 
+/**
+ * @brief Namespace containing all the the mcpp library classes.
+ *
+ * The mcpp namespace includes classes and functions designed to facilitate
+ * interaction with the Minecraft world through various server commands
+ * and data manipulations.
+ */
 namespace mcpp {
 class MinecraftConnection {
   private:
-    std::unique_ptr<SocketConnection> conn;
+    std::unique_ptr<SocketConnection> conn; ///< Handle to the socket connection.
 
     static std::vector<std::vector<std::vector<BlockType>>>
     unflattenBlocksArray(const Coordinate& loc1, const Coordinate& loc2,
-                         const std::vector<BlockType>& inVector);
+                         const std::vector<BlockType>& inVector); ///< Helper function to convert flat block array to 3D.
 
     static std::vector<std::vector<int>>
     unflattenHeightsArray(const Coordinate& loc1, const Coordinate& loc2,
-                          const std::vector<int>& inVector);
+                          const std::vector<int>& inVector); ///< Helper function to convert flat height array to 2D.
 
   public:
     /**
-     * Represents the main endpoint for interaction with the minecraft world.
+     * @brief Represents the main endpoint for interaction with the minecraft world.
+     * 
      * @param address String address in IPV4 format, defaults to "localhost"
      * @param port Integer port to run on, defaults to 4711 as that is the port
      * for ELCI
@@ -31,55 +43,63 @@ class MinecraftConnection {
                                  int port = 4711);
 
     /**
-     * Sends a message to the in-game chat, does not require a joined player
+     * @brief Sends a message to the in-game chat, does not require a joined player
+     * 
      * @param message
      */
     void postToChat(const std::string& message);
 
     /**
-     * Performs an in-game minecraft command. Players have to exist on the
+     * @brief Performs an in-game minecraft command. Players have to exist on the
      * server and should be server operators (default with ELCI)
+     * 
      * @param command Command string in the in-game format (e.g. "time set day")
      */
     void doCommand(const std::string& command);
 
     /**
-     * Sets player pos (block pos of lower half of playermodel) to specified
+     * @brief Sets player pos (block pos of lower half of playermodel) to specified
      * Coordinate
+     * 
      * @param pos Coordinate to set
      */
     void setPlayerPosition(const Coordinate& pos);
 
     /**
-     * Returns a coordinate representing player position (block pos of lower
+     * @brief Returns a coordinate representing player position (block pos of lower
      * half of playermodel)
+     * 
      * @return Coordinate of location
      */
     Coordinate getPlayerPosition();
 
     /**
-     * Sets player position to be one above specified tile (i.e. tile = block
+     * @brief Sets player position to be one above specified tile (i.e. tile = block
      * player is standing on)
+     * 
      * @param tile Coordinate to set
      */
     void setPlayerTilePosition(const Coordinate& tile);
 
     /**
-     * Returns the coordinate location of the block the player is standing on
+     * @brief Returns the coordinate location of the block the player is standing on
+     * 
      * @return Coordinate of location
      */
     Coordinate getPlayerTilePosition();
 
     /**
-     * Sets block at Coordinate loc to the BlockType specified by blockType
+     * @brief Sets block at Coordinate loc to the BlockType specified by blockType
+     * 
      * @param loc
      * @param blockType
      */
     void setBlock(const Coordinate& loc, const BlockType& blockType);
 
     /**
-     * Sets a cuboid of blocks to the specified BlockType blockType, with the
+     * @brief Sets a cuboid of blocks to the specified BlockType blockType, with the
      * corners of the cuboid provided by the Coordinate loc1 and loc2
+     * 
      * @param loc1
      * @param loc2
      * @param blockType
@@ -88,15 +108,17 @@ class MinecraftConnection {
                    const BlockType& blockType);
 
     /**
-     * Returns BlockType object from the specified Coordinate loc with modifier
+     * @brief Returns BlockType object from the specified Coordinate loc with modifier
+     * 
      * @param loc
      * @return BlockType of the requested block
      */
     BlockType getBlock(const Coordinate& loc);
 
     /**
-     * Returns a 3D vector of the BlockTypes of the requested cuboid with
+     * @brief Returns a 3D vector of the BlockTypes of the requested cuboid with
      * modifiers
+     * 
      * @param loc1 1st corner of the cuboid
      * @param loc2 2nd corner of the cuboid
      * @return 3D vector of BlockType in the specified cuboid.
@@ -105,8 +127,12 @@ class MinecraftConnection {
     getBlocks(const Coordinate& loc1, const Coordinate& loc2);
 
     /**
-     * IMPORTANT: DO NOT USE FOR LARGE AREAS, IT WILL BE VERY SLOW
+     * @brief Returns the height of the specific provided x and y coordinate
+     * 
+     * ***IMPORTANT:***
+     * DO NOT USE FOR LARGE AREAS, IT WILL BE VERY SLOW
      * USE getHeights() INSTEAD
+     * 
      * Gets the y-value of the highest non-air block at the specified (x, z)
      * coordinate.
      * @param x
@@ -116,8 +142,11 @@ class MinecraftConnection {
     int getHeight(int x, int z);
 
     /**
-     * Provides a scaled option of the getHeight call to allow for considerable
-     * performance gains. USE THIS instead of getHeight in a for loop.
+     * @brief Provides a scaled option of the getHeight call to allow for considerable
+     * performance gains. 
+     * 
+     * \par USE THIS instead of getHeight in a for loop.
+     * 
      * @param loc1
      * @param loc2
      * @return Returns a vector of integers representing the 2D area of heights.
