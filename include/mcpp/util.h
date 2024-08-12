8cf87bddf942f9e608a7843533a9b1a149a7ee7e
nhatdongdang <144138246+nhatdongdang@users.noreply.github.com>
2024-08-12 14:25:01 +1000

    First iterator version

--- include/mcpp/util.h
+++ include/mcpp/util.h
@@ -132,6 +132,43 @@ struct Chunk {
  * non-air blocks at each (x,z)
  */
 struct HeightMap {
+    struct Iterator {
+        using value_type = int;
+        using pointer = int*;
+        using reference = int&;
+
+        Iterator(pointer ptr) : m_ptr(ptr) {}
+
+        reference operator*() const { return *m_ptr; }
+        pointer operator->() { return m_ptr; }
+
+        // Prefix increment
+        Iterator& operator++() {
+            m_ptr++;
+            return *this;
+        }
+
+        // Postfix increment
+        Iterator operator++(int) {
+            Iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        friend bool operator==(const Iterator& a, const Iterator& b) {
+            return a.m_ptr == b.m_ptr;
+        };
+
+        friend bool operator!=(const Iterator& a, const Iterator& b) {
+            return a.m_ptr != b.m_ptr;
+        };
+
+      private:
+        pointer m_ptr;
+    };
+    Iterator begin() { return Iterator(&raw_heights[0]); }
+    Iterator end() { return Iterator(&raw_heights[_x_len * _z_len]); }
+
     HeightMap(const Coordinate& loc1, const Coordinate& loc2,
               const std::vector<int>& heights);
 
