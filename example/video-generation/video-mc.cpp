cde0c44ec0de8c72c703e1df3d9ba78a8152f6f7
Tom Lynch <twhlynch@gmail.com>
2024-09-26 18:07:38 +1000

    auto format

--- example/video-generation/video-mc.cpp
+++ example/video-generation/video-mc.cpp
@@ -1,60 +1,60 @@
-#include <mcpp/mcpp.h>
-#include <string>
-#include <vector>
+#include <chrono>
 #include <fstream>
 #include <iostream>
+#include <mcpp/mcpp.h>
+#include <string>
 #include <thread>
-#include <chrono>
+#include <vector>
 
-struct Pixel
-{
+struct Pixel {
     uint8_t r, g, b;
 };
 
-class Video
-{
-    public:
-        Video(const std::string &filename, int width, int height, int scaleFactor, int frameRate);
-        ~Video()
-        {
-            if(_file.is_open())
-            {
-                _file.close();
-            }
+class Video {
+  public:
+    Video(const std::string& filename, int width, int height, int scaleFactor,
+          int frameRate);
+    ~Video() {
+        if (_file.is_open()) {
+            _file.close();
         }
+    }
 
-        void Play(mcpp::MinecraftConnection &mc);
+    void Play(mcpp::MinecraftConnection& mc);
 
-    private:
-        void DisplayFrame(size_t index, mcpp::MinecraftConnection &mc);
-        mcpp::BlockType GetBestBlock(const Pixel& pixel);
+  private:
+    void DisplayFrame(size_t index, mcpp::MinecraftConnection& mc);
+    mcpp::BlockType GetBestBlock(const Pixel& pixel);
 
-        std::ifstream _file;
-        std::vector<std::vector<Pixel>> _frames;
+    std::ifstream _file;
+    std::vector<std::vector<Pixel>> _frames;
 
-        int _width;
-        int _height;
-        int _scaleFactor;
-        int _frameRate;
-        int _frameDelay;
-        int _frameSize;
+    int _width;
+    int _height;
+    int _scaleFactor;
+    int _frameRate;
+    int _frameDelay;
+    int _frameSize;
 
-        mcpp::Coordinate _position;
-        std::vector<mcpp::BlockType> _options;
-        std::vector<Pixel> _optionColors;
+    mcpp::Coordinate _position;
+    std::vector<mcpp::BlockType> _options;
+    std::vector<Pixel> _optionColors;
 };
 
-int main(int argc, char *argv[]) {
-    if(argc < 6)
-    {
-        std::cerr << "Usage: " << argv[0] << " filename.rgb width height scale frame_rate" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 6) {
+        std::cerr << "Usage: " << argv[0]
+                  << " filename.rgb width height scale frame_rate" << std::endl;
         return 1;
     }
 
     std::string filename = argv[1];
-    if(filename.find(".mp4") != std::string::npos)
-    {
-        std::cerr << "Be sure to convert mp4 files to rgb files before use." << std::endl << "Usage: ffmpeg -i filename.mp4 -f rawvideo -pix_fmt rgb24 filename.rgb" << std::endl;
+    if (filename.find(".mp4") != std::string::npos) {
+        std::cerr << "Be sure to convert mp4 files to rgb files before use."
+                  << std::endl
+                  << "Usage: ffmpeg -i filename.mp4 -f rawvideo -pix_fmt rgb24 "
+                     "filename.rgb"
+                  << std::endl;
         return 1;
     }
 
@@ -65,41 +65,36 @@ int main(int argc, char *argv[]) {
 
     mcpp::MinecraftConnection mc;
 
-    Video *video = new Video(filename, width, height, scaleFactor, frameRate);
+    Video* video = new Video(filename, width, height, scaleFactor, frameRate);
     video->Play(mc);
 
     return 0;
 }
 
-Video::Video(const std::string &filename, int width, int height, int scaleFactor, int frameRate)
-: _width(width), _height(height), _scaleFactor(scaleFactor), _frameRate(frameRate), 
-_frameDelay(1000 / _frameRate), _frameSize(_width * _height * 3)
-{
+Video::Video(const std::string& filename, int width, int height,
+             int scaleFactor, int frameRate)
+    : _width(width), _height(height), _scaleFactor(scaleFactor),
+      _frameRate(frameRate), _frameDelay(1000 / _frameRate),
+      _frameSize(_width * _height * 3) {
     _file.open(filename, std::ios::binary);
-    if(!_file.is_open())
-    {
+    if (!_file.is_open()) {
         throw std::runtime_error("Error opening file");
     }
-    
-    while(_file)
-    {
+
+    while (_file) {
         std::vector<Pixel> frame;
         frame.resize(_width * _height);
         _file.read(reinterpret_cast<char*>(frame.data()), _frameSize);
 
-        if(_file.gcount() == _frameSize)
-        {
+        if (_file.gcount() == _frameSize) {
             _frames.push_back(frame);
-        }
-        else
-        {
+        } else {
             break;
         }
     }
 }
 
-void Video::Play(mcpp::MinecraftConnection &mc)
-{
+void Video::Play(mcpp::MinecraftConnection& mc) {
     _options.push_back(mcpp::Blocks::RED_CONCRETE);
     _options.push_back(mcpp::Blocks::BLUE_CONCRETE);
     _options.push_back(mcpp::Blocks::CYAN_CONCRETE);
@@ -133,73 +128,64 @@ void Video::Play(mcpp::MinecraftConnection &mc)
     _options.push_back(mcpp::Blocks::MAGENTA_WOOL);
     _options.push_back(mcpp::Blocks::LIGHT_BLUE_WOOL);
     _options.push_back(mcpp::Blocks::LIGHT_GRAY_WOOL);
-    
-    _optionColors.push_back(Pixel{142, 33, 33}); // RED CONCRETE
-    _optionColors.push_back(Pixel{45, 47, 143}); // BLUE CONCRETE
-    _optionColors.push_back(Pixel{21, 119, 136}); // CYAN CONCRETE
-    _optionColors.push_back(Pixel{55, 58, 62}); // GRAY CONCRETE
-    _optionColors.push_back(Pixel{94, 169, 24}); // LIME CONCRETE
+
+    _optionColors.push_back(Pixel{142, 33, 33});   // RED CONCRETE
+    _optionColors.push_back(Pixel{45, 47, 143});   // BLUE CONCRETE
+    _optionColors.push_back(Pixel{21, 119, 136});  // CYAN CONCRETE
+    _optionColors.push_back(Pixel{55, 58, 62});    // GRAY CONCRETE
+    _optionColors.push_back(Pixel{94, 169, 24});   // LIME CONCRETE
     _optionColors.push_back(Pixel{213, 101, 143}); // PINK CONCRETE
-    _optionColors.push_back(Pixel{8, 10, 15}); // BLACK CONCRETE
-    _optionColors.push_back(Pixel{96, 60, 32}); // BROWN CONCRETE
-    _optionColors.push_back(Pixel{73, 91, 36}); // GREEN CONCRETE
+    _optionColors.push_back(Pixel{8, 10, 15});     // BLACK CONCRETE
+    _optionColors.push_back(Pixel{96, 60, 32});    // BROWN CONCRETE
+    _optionColors.push_back(Pixel{73, 91, 36});    // GREEN CONCRETE
     _optionColors.push_back(Pixel{255, 255, 255}); // WHITE CONCRETE
-    _optionColors.push_back(Pixel{224, 97, 1}); // ORANGE CONCRETE
-    _optionColors.push_back(Pixel{100, 32, 156}); // PURPLE CONCRETE
-    _optionColors.push_back(Pixel{241, 175, 21}); // YELLOW CONCRETE
-    _optionColors.push_back(Pixel{169, 48, 159}); // MAGENTA CONCRETE
-    _optionColors.push_back(Pixel{36, 137, 199}); // LIGHT BLUE CONCRETE
+    _optionColors.push_back(Pixel{224, 97, 1});    // ORANGE CONCRETE
+    _optionColors.push_back(Pixel{100, 32, 156});  // PURPLE CONCRETE
+    _optionColors.push_back(Pixel{241, 175, 21});  // YELLOW CONCRETE
+    _optionColors.push_back(Pixel{169, 48, 159});  // MAGENTA CONCRETE
+    _optionColors.push_back(Pixel{36, 137, 199});  // LIGHT BLUE CONCRETE
     _optionColors.push_back(Pixel{125, 125, 115}); // LIGHT GRAY CONCRETE
 
-    _optionColors.push_back(Pixel{158, 43, 39}); // RED WOOL
-    _optionColors.push_back(Pixel{37, 49, 147}); // BLUE WOOL
-    _optionColors.push_back(Pixel{38, 113, 145}); // CYAN WOOL
-    _optionColors.push_back(Pixel{65, 65, 65}); // GRAY WOOL
-    _optionColors.push_back(Pixel{57, 186, 46}); // LIME WOOL
+    _optionColors.push_back(Pixel{158, 43, 39});   // RED WOOL
+    _optionColors.push_back(Pixel{37, 49, 147});   // BLUE WOOL
+    _optionColors.push_back(Pixel{38, 113, 145});  // CYAN WOOL
+    _optionColors.push_back(Pixel{65, 65, 65});    // GRAY WOOL
+    _optionColors.push_back(Pixel{57, 186, 46});   // LIME WOOL
     _optionColors.push_back(Pixel{217, 129, 153}); // PINK WOOL
-    _optionColors.push_back(Pixel{24, 20, 20}); // BLACK WOOL
-    _optionColors.push_back(Pixel{86, 51, 28}); // BROWN WOOL
-    _optionColors.push_back(Pixel{54, 75, 24}); // GREEN WOOL
+    _optionColors.push_back(Pixel{24, 20, 20});    // BLACK WOOL
+    _optionColors.push_back(Pixel{86, 51, 28});    // BROWN WOOL
+    _optionColors.push_back(Pixel{54, 75, 24});    // GREEN WOOL
     _optionColors.push_back(Pixel{228, 228, 228}); // WHITE WOOL
-    _optionColors.push_back(Pixel{235, 126, 54}); // ORANGE WOOL
-    _optionColors.push_back(Pixel{126, 52, 191}); // PURPLE WOOL
-    _optionColors.push_back(Pixel{194, 181, 28}); // YELLOW WOOL
-    _optionColors.push_back(Pixel{190, 73, 201}); // MAGENTA WOOL
+    _optionColors.push_back(Pixel{235, 126, 54});  // ORANGE WOOL
+    _optionColors.push_back(Pixel{126, 52, 191});  // PURPLE WOOL
+    _optionColors.push_back(Pixel{194, 181, 28});  // YELLOW WOOL
+    _optionColors.push_back(Pixel{190, 73, 201});  // MAGENTA WOOL
     _optionColors.push_back(Pixel{228, 236, 253}); // LIGHT BLUE WOOL
     _optionColors.push_back(Pixel{160, 167, 167}); // LIGHT GRAY WOOL
 
     _position = mc.getPlayerPosition();
     _position.y += (_height / _scaleFactor) / 2;
-    _position.z -= std::max((_width / _scaleFactor) / 2, (_height / _scaleFactor) / 2);
+    _position.z -=
+        std::max((_width / _scaleFactor) / 2, (_height / _scaleFactor) / 2);
     _position.x += std::min((_width / _scaleFactor) / 2, 16);
 
-    for(size_t i = 0; i < _frames.size(); i++)
-    {
+    for (size_t i = 0; i < _frames.size(); i++) {
         DisplayFrame(i, mc);
         std::this_thread::sleep_for(std::chrono::milliseconds(_frameDelay));
     }
 }
 
-void Video::DisplayFrame(size_t index, mcpp::MinecraftConnection &mc)
-{
-    if(index < _frames.size())
-    {
+void Video::DisplayFrame(size_t index, mcpp::MinecraftConnection& mc) {
+    if (index < _frames.size()) {
         const std::vector<Pixel>& frame = _frames[index];
 
-        if(!frame.empty())
-        {
-            for(size_t i = 0; i < frame.size(); i += _scaleFactor)
-            {
+        if (!frame.empty()) {
+            for (size_t i = 0; i < frame.size(); i += _scaleFactor) {
                 const Pixel& pixel = frame[i];
-                if(index > 0)
-                {
+                if (index > 0) {
                     const Pixel& lastPixel = _frames[index - 1][i];
-                    if(
-                        pixel.r == lastPixel.r
-                        && pixel.g == lastPixel.g
-                        && pixel.b == lastPixel.b
-                    )
-                    {
+                    if (pixel.r == lastPixel.r && pixel.g == lastPixel.g &&
+                        pixel.b == lastPixel.b) {
                         continue;
                     }
                 }
@@ -216,8 +202,7 @@ void Video::DisplayFrame(size_t index, mcpp::MinecraftConnection &mc)
     }
 }
 
-mcpp::BlockType Video::GetBestBlock(const Pixel& pixel)
-{
+mcpp::BlockType Video::GetBestBlock(const Pixel& pixel) {
     int r = pixel.r;
     int g = pixel.g;
     int b = pixel.b;
@@ -225,27 +210,22 @@ mcpp::BlockType Video::GetBestBlock(const Pixel& pixel)
     int minDistance = 256;
     int closestOption = -1;
 
-    for(size_t i = 0; i < _optionColors.size(); i++)
-    {
+    for (size_t i = 0; i < _optionColors.size(); i++) {
         int dr = std::abs(r - _optionColors[i].r);
         int dg = std::abs(g - _optionColors[i].g);
         int db = std::abs(b - _optionColors[i].b);
 
         int distance = std::sqrt(dr * dr + dg * dg + db * db);
 
-        if(distance < minDistance)
-        {
+        if (distance < minDistance) {
             minDistance = distance;
             closestOption = i;
         }
     }
 
-    if(closestOption!= -1)
-    {
+    if (closestOption != -1) {
         return _options[closestOption];
-    }
-    else
-    {
+    } else {
         std::cerr << "Color error" << std::endl;
     }
 
