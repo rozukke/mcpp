2f1c35afbf28099e7564a8917b282c765e40312a
Mitch <s4018169@student.rmit.edu.au>
2024-10-01 11:06:31 +1000

    Run clang-format

--- example/minesweeper/minesweeper.cpp
+++ example/minesweeper/minesweeper.cpp
@@ -1,22 +1,23 @@
-#include <mcpp/mcpp.h>
 #include <mcpp/block.h>
-#include <vector>
+#include <mcpp/mcpp.h>
 #include <string>
+#include <vector>
 
 int main() {
 
-    const int size_x = 10; 
+    const int size_x = 10;
     const int size_z = 10;
     const int minecount = 15;
 
-    long int seed = static_cast<long int> (time(NULL));
+    long int seed = static_cast<long int>(time(NULL));
 
     int flagToWin = minecount;
     int clearsToWin = (size_x * size_z) - minecount;
 
     srand(seed);
 
-    int board[size_x][size_z]; // Defaults board states to 0 (meaning no mines nearby)
+    int board[size_x]
+             [size_z]; // Defaults board states to 0 (meaning no mines nearby)
     int state[size_x][size_z]; // And 0 meaning no clear/flag
     for (int x = 0; x < size_x; x++) {
         for (int z = 0; z < size_z; z++) {
@@ -25,14 +26,12 @@ int main() {
         }
     }
 
-
     mcpp::MinecraftConnection mc;
-    mcpp::Coordinate orig; // hols the location of the board origin
+    mcpp::Coordinate orig;   // hols the location of the board origin
     mcpp::Coordinate corner; // holds the location of the opposite corner
-    mcpp::Coordinate tpos; // used for temporary positions
+    mcpp::Coordinate tpos;   // used for temporary positions
     mcpp::Coordinate cnr1;
     mcpp::Coordinate cnr2;
-
 
     mc.doCommand("gamerule sendCommandFeedback false");
     mc.doCommand("clear @p");
@@ -46,45 +45,48 @@ int main() {
     mc.postToChat("The barrier exits the game");
     mc.postToChat("To play, place blocks above the board");
 
-    
-    std::vector<mcpp::BlockType> minecols = 
-        {mcpp::Blocks::WHITE_CONCRETE, mcpp::Blocks::LIGHT_BLUE_CONCRETE, mcpp::Blocks::CYAN_CONCRETE,
-         mcpp::Blocks::GREEN_CONCRETE, mcpp::Blocks::LIME_CONCRETE, mcpp::Blocks::YELLOW_CONCRETE,
-         mcpp::Blocks::ORANGE_CONCRETE, mcpp::Blocks::RED_CONCRETE, mcpp::Blocks::BROWN_CONCRETE, mcpp::Blocks::TNT};
+    std::vector<mcpp::BlockType> minecols = {
+        mcpp::Blocks::WHITE_CONCRETE,  mcpp::Blocks::LIGHT_BLUE_CONCRETE,
+        mcpp::Blocks::CYAN_CONCRETE,   mcpp::Blocks::GREEN_CONCRETE,
+        mcpp::Blocks::LIME_CONCRETE,   mcpp::Blocks::YELLOW_CONCRETE,
+        mcpp::Blocks::ORANGE_CONCRETE, mcpp::Blocks::RED_CONCRETE,
+        mcpp::Blocks::BROWN_CONCRETE,  mcpp::Blocks::TNT};
 
     orig = mc.getPlayerTilePosition();
 
     corner = orig;
-    corner.x += size_x-1;
-    corner.z += size_z-1;
+    corner.x += size_x - 1;
+    corner.z += size_z - 1;
 
     tpos = orig;
     tpos.z--;
     tpos.y++;
-    for (int i = 1; i < 10; i++){
-        mc.setBlock(tpos,minecols[i]);
+    for (int i = 1; i < 10; i++) {
+        mc.setBlock(tpos, minecols[i]);
         tpos.x++;
     }
-    mc.setBlock(tpos,mcpp::Blocks::SEA_LANTERN);
+    mc.setBlock(tpos, mcpp::Blocks::SEA_LANTERN);
     tpos.y++;
-    mc.setBlock(tpos,mcpp::Blocks::GLOWSTONE);
+    mc.setBlock(tpos, mcpp::Blocks::GLOWSTONE);
     tpos.x--;
-    mc.setBlock(tpos,mcpp::Blocks::EMERALD_BLOCK);
+    mc.setBlock(tpos, mcpp::Blocks::EMERALD_BLOCK);
 
-    mc.setBlocks(orig,corner,mcpp::Blocks::LIGHT_GRAY_CONCRETE);
-    
+    mc.setBlocks(orig, corner, mcpp::Blocks::LIGHT_GRAY_CONCRETE);
+
     for (int i = 0; i < minecount; i++) {
         int x = rand() % size_x;
         int z = rand() % size_z;
 
-        if (board[x][z] == 9) i--;
+        if (board[x][z] == 9)
+            i--;
         else {
             board[x][z] = 9;
-            for (int xmod = -1; xmod < 2; xmod++){
-                for (int zmod = -1; zmod < 2; zmod++){
-                    if ((x + xmod) >= 0 && (x + xmod) < size_x && (z + zmod) >= 0 && (z + zmod) < size_z ) {
-                        if (board[x+xmod][z+zmod] != 9) {
-                            board[x+xmod][z+zmod] += 1;
+            for (int xmod = -1; xmod < 2; xmod++) {
+                for (int zmod = -1; zmod < 2; zmod++) {
+                    if ((x + xmod) >= 0 && (x + xmod) < size_x &&
+                        (z + zmod) >= 0 && (z + zmod) < size_z) {
+                        if (board[x + xmod][z + zmod] != 9) {
+                            board[x + xmod][z + zmod] += 1;
                         }
                     }
                 }
@@ -100,28 +102,27 @@ int main() {
     bool gameRunning = true;
 
     while (gameRunning) {
-        mcpp::Chunk game = mc.getBlocks(cnr1,cnr2);
+        mcpp::Chunk game = mc.getBlocks(cnr1, cnr2);
         for (int x = 0; x < size_x; x++) {
-            for (int z = 0; z < size_z; z++){
-                
-                if (game.get(x,0,z) == mcpp::Blocks::TNT){
+            for (int z = 0; z < size_z; z++) {
+
+                if (game.get(x, 0, z) == mcpp::Blocks::TNT) {
                     tpos.x = orig.x + x;
                     tpos.y = orig.y;
                     tpos.z = orig.z + z;
                     tpos.y++;
-                    mc.setBlock(tpos,mcpp::Blocks::AIR);
+                    mc.setBlock(tpos, mcpp::Blocks::AIR);
                     tpos.y--;
 
                     if (state[x][z] == 0) {
                         clearsToWin--;
                         state[x][z] = 1;
-                        mc.setBlock(tpos,minecols.at(board[x][z]));
-                        
+                        mc.setBlock(tpos, minecols.at(board[x][z]));
+
                         if (board[x][z] == 9) {
                             mc.postToChat("GameOver");
                             gameRunning = false;
-                        }
-                        else if (board[x][z] == 0) {
+                        } else if (board[x][z] == 0) {
                             std::vector<mcpp::Coordinate> zc;
                             zc.push_back(tpos);
                             mc.postToChat("Zero Clear");
@@ -129,54 +130,66 @@ int main() {
                             for (size_t pos = 0; pos < zc.size(); pos++) {
                                 mcpp::Coordinate cord = zc.at(pos);
                                 for (int xmod = -1; xmod <= 1; xmod++) {
-                                    for (int zmod = -1; zmod <= 1; zmod++){
-                                        if (cord.x + xmod >= orig.x && cord.x + xmod <= corner.x && cord.z + zmod >= orig.z && cord.z + zmod <= corner.z ){
+                                    for (int zmod = -1; zmod <= 1; zmod++) {
+                                        if (cord.x + xmod >= orig.x &&
+                                            cord.x + xmod <= corner.x &&
+                                            cord.z + zmod >= orig.z &&
+                                            cord.z + zmod <= corner.z) {
                                             tpos = cord;
                                             tpos.x += xmod;
                                             tpos.z += zmod;
 
-                                            if (state[tpos.x-orig.x][tpos.z-orig.z] != 1) {
+                                            if (state[tpos.x - orig.x]
+                                                     [tpos.z - orig.z] != 1) {
                                                 clearsToWin--;
-                                                if (board[tpos.x-orig.x][tpos.z-orig.z] == 0) {
+                                                if (board[tpos.x - orig.x]
+                                                         [tpos.z - orig.z] ==
+                                                    0) {
                                                     zc.push_back(tpos);
                                                 }
                                             }
-                                            mc.setBlock(tpos,minecols.at(board[tpos.x-orig.x][tpos.z-orig.z]));
-                                            state[tpos.x-orig.x][tpos.z-orig.z] = 1;
+                                            mc.setBlock(
+                                                tpos,
+                                                minecols.at(
+                                                    board[tpos.x - orig.x]
+                                                         [tpos.z - orig.z]));
+                                            state[tpos.x - orig.x]
+                                                 [tpos.z - orig.z] = 1;
                                         }
                                     }
                                 }
                             }
                         }
-                    }                            
-                }
-                else if (game.get(x,0,z) == mcpp::Blocks::SEA_LANTERN) {
+                    }
+                } else if (game.get(x, 0, z) == mcpp::Blocks::SEA_LANTERN) {
                     tpos.x = orig.x + x;
                     tpos.y = orig.y;
                     tpos.z = orig.z + z;
 
-                    if (state[x][z] == 0){
+                    if (state[x][z] == 0) {
                         state[x][z] = 2;
-                        mc.setBlock(tpos,mcpp::Blocks::SEA_LANTERN);
-                        if (board[x][z] == 9) flagToWin--;
-                        else flagToWin++;
-                    }
-                    else if (state[x][z] == 2) {
+                        mc.setBlock(tpos, mcpp::Blocks::SEA_LANTERN);
+                        if (board[x][z] == 9)
+                            flagToWin--;
+                        else
+                            flagToWin++;
+                    } else if (state[x][z] == 2) {
                         state[x][z] = 0;
-                        mc.setBlock(tpos,mcpp::Blocks::LIGHT_GRAY_CONCRETE);
-                        if (board[x][z] == 9) flagToWin++;
-                        else flagToWin--;
+                        mc.setBlock(tpos, mcpp::Blocks::LIGHT_GRAY_CONCRETE);
+                        if (board[x][z] == 9)
+                            flagToWin++;
+                        else
+                            flagToWin--;
                     }
 
                     tpos.y++;
-                    mc.setBlock(tpos,mcpp::Blocks::AIR);
-                }
-                else if (game.get(x,0,z) == mcpp::Blocks::BARRIER) {
+                    mc.setBlock(tpos, mcpp::Blocks::AIR);
+                } else if (game.get(x, 0, z) == mcpp::Blocks::BARRIER) {
                     tpos.x = orig.x + x;
                     tpos.y = orig.y;
                     tpos.z = orig.z + z;
                     tpos.y++;
-                    mc.setBlock(tpos,mcpp::Blocks::AIR);
+                    mc.setBlock(tpos, mcpp::Blocks::AIR);
                     gameRunning = false;
                     mc.postToChat("Quitting");
                 }
@@ -199,14 +212,12 @@ int main() {
 
             if (state[x][z] == 2) {
                 if (board[x][z] == 9) {
-                    mc.setBlock(tpos,mcpp::Blocks::EMERALD_BLOCK);
+                    mc.setBlock(tpos, mcpp::Blocks::EMERALD_BLOCK);
+                } else {
+                    mc.setBlock(tpos, mcpp::Blocks::GLOWSTONE);
                 }
-                else {
-                    mc.setBlock(tpos,mcpp::Blocks::GLOWSTONE);
-                }
-            }
-            else if (board[x][z] == 9){
-                mc.setBlock(tpos,mcpp::Blocks::TNT);
+            } else if (board[x][z] == 9) {
+                mc.setBlock(tpos, mcpp::Blocks::TNT);
             }
         }
     }
