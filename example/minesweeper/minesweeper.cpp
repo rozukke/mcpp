8ad472eb66d7dc6b6e13f83730769e4890647bb0
Mitch <s4018169@student.rmit.edu.au>
2024-10-01 11:19:10 +1000

    Cleaning up code before merge, please wait until final commit

--- example/minesweeper/minesweeper.cpp
+++ example/minesweeper/minesweeper.cpp
@@ -1,38 +1,36 @@
-#include <mcpp/block.h>
 #include <mcpp/mcpp.h>
-#include <string>
-#include <vector>
+
+#define X_SIZE 100
+#define Z_SIZE 100
+#define MINE_COUNT 15
+
+#define clear mcpp::Blocks::TNT
+#define flag mcpp::Blocks::SEA_LANTERN
+#define uncleared mcpp::Blocks::LIGHT_GRAY_CONCRETE
+#define quit mcpp::Blocks::BARRIER
+#define air mcpp::Blocks::AIR
+#define goodmine mcpp::Blocks::EMERALD_BLOCK
+#define badflag mcpp::Blocks::GLOWSTONE
+#define badmine mcpp::Blocks::MAGMA_BLOCK
 
 int main() {
-
-    const int size_x = 10;
-    const int size_z = 10;
-    const int minecount = 15;
-
-    long int seed = static_cast<long int>(time(NULL));
-
-    int flagToWin = minecount;
-    int clearsToWin = (size_x * size_z) - minecount;
-
-    srand(seed);
-
-    int board[size_x]
-             [size_z]; // Defaults board states to 0 (meaning no mines nearby)
-    int state[size_x][size_z]; // And 0 meaning no clear/flag
-    for (int x = 0; x < size_x; x++) {
-        for (int z = 0; z < size_z; z++) {
-            board[x][z] = 0;
-            state[x][z] = 0;
-        }
-    }
-
     mcpp::MinecraftConnection mc;
-    mcpp::Coordinate orig;   // hols the location of the board origin
-    mcpp::Coordinate corner; // holds the location of the opposite corner
-    mcpp::Coordinate tpos;   // used for temporary positions
-    mcpp::Coordinate cnr1;
-    mcpp::Coordinate cnr2;
+    mcpp::Coordinate origin = mc.getPlayerTilePosition();
+    srand(time(nullptr));
 
+    // The third layer to the board has mine values in [0] and flag/clears in
+    // [1]
+    int board[X_SIZE][Z_SIZE][2] = {{{0}}};
+
+    // Used later for the colours of the blocks, values 0-9
+    std::vector<mcpp::BlockType> blocks = {
+        mcpp::Blocks::WHITE_CONCRETE,  mcpp::Blocks::LIGHT_BLUE_CONCRETE,
+        mcpp::Blocks::CYAN_CONCRETE,   mcpp::Blocks::GREEN_CONCRETE,
+        mcpp::Blocks::LIME_CONCRETE,   mcpp::Blocks::YELLOW_CONCRETE,
+        mcpp::Blocks::ORANGE_CONCRETE, mcpp::Blocks::RED_CONCRETE,
+        mcpp::Blocks::BROWN_CONCRETE,  mcpp::Blocks::TNT};
+
+    // Gives the player required items and lets them know how to play
     mc.doCommand("gamerule sendCommandFeedback false");
     mc.doCommand("clear @p");
     mc.doCommand("give @p tnt");
@@ -45,180 +43,197 @@ int main() {
     mc.postToChat("The barrier exits the game");
     mc.postToChat("To play, place blocks above the board");
 
-    std::vector<mcpp::BlockType> minecols = {
-        mcpp::Blocks::WHITE_CONCRETE,  mcpp::Blocks::LIGHT_BLUE_CONCRETE,
-        mcpp::Blocks::CYAN_CONCRETE,   mcpp::Blocks::GREEN_CONCRETE,
-        mcpp::Blocks::LIME_CONCRETE,   mcpp::Blocks::YELLOW_CONCRETE,
-        mcpp::Blocks::ORANGE_CONCRETE, mcpp::Blocks::RED_CONCRETE,
-        mcpp::Blocks::BROWN_CONCRETE,  mcpp::Blocks::TNT};
+    // Makes the board and legend
+    mcpp::Coordinate cornerOrigin = origin;
+    mcpp::Coordinate cornerOpposite = origin;
 
-    orig = mc.getPlayerTilePosition();
+    cornerOpposite.x += X_SIZE - 1;
+    cornerOpposite.z += Z_SIZE - 1;
+    mc.setBlocks(cornerOrigin, cornerOpposite, uncleared);
 
-    corner = orig;
-    corner.x += size_x - 1;
-    corner.z += size_z - 1;
+    // Moves corners up for reuse later to get the board
+    cornerOpposite.y++;
+    cornerOrigin.y++;
 
-    tpos = orig;
-    tpos.z--;
-    tpos.y++;
-    for (int i = 1; i < 10; i++) {
-        mc.setBlock(tpos, minecols[i]);
-        tpos.x++;
+    mcpp::Coordinate legendPrinter = origin;
+    legendPrinter.z--;
+    legendPrinter.y++;
+    for (int i = 0; i <= 9; i++) {
+        mc.setBlock(legendPrinter, blocks[i]);
+        legendPrinter.x++;
     }
-    mc.setBlock(tpos, mcpp::Blocks::SEA_LANTERN);
-    tpos.y++;
-    mc.setBlock(tpos, mcpp::Blocks::GLOWSTONE);
-    tpos.x--;
-    mc.setBlock(tpos, mcpp::Blocks::EMERALD_BLOCK);
+    mc.setBlock(legendPrinter, flag);
+    legendPrinter.y++;
+    mc.setBlock(legendPrinter, badflag);
+    legendPrinter.x--;
+    mc.setBlock(legendPrinter, goodmine);
+    legendPrinter.y++;
+    mc.setBlock(legendPrinter, badmine);
 
-    mc.setBlocks(orig, corner, mcpp::Blocks::LIGHT_GRAY_CONCRETE);
+    // Makes the mines and increases the adjacent counts by 1
+    for (int mines = 0; mines < MINE_COUNT; mines++) {
+        int x = rand() % X_SIZE;
+        int z = rand() % Z_SIZE;
 
-    for (int i = 0; i < minecount; i++) {
-        int x = rand() % size_x;
-        int z = rand() % size_z;
+        if (board[x][z][0] !=
+            9) {                // Select a random place and if its not a mine
+            board[x][z][0] = 9; // Make it a mine
 
-        if (board[x][z] == 9)
-            i--;
-        else {
-            board[x][z] = 9;
-            for (int xmod = -1; xmod < 2; xmod++) {
-                for (int zmod = -1; zmod < 2; zmod++) {
-                    if ((x + xmod) >= 0 && (x + xmod) < size_x &&
-                        (z + zmod) >= 0 && (z + zmod) < size_z) {
-                        if (board[x + xmod][z + zmod] != 9) {
-                            board[x + xmod][z + zmod] += 1;
+            for (int xOfset = -1; xOfset <= 1;
+                 xOfset++) { // For area 3x3 around mine
+                if (x + xOfset >= 0 &&
+                    x + xOfset < X_SIZE) { // if not out of bounds or
+                    for (int zOfset = -1; zOfset <= 1;
+                         zOfset++) { // a mine increase the count
+                        if (z + zOfset >= 0 &&
+                            z + zOfset < Z_SIZE) { // by 1. Area checked :
+                            if (board[x + xOfset][z + zOfset][0] !=
+                                9) {                                //  + + +
+                                board[x + xOfset][z + zOfset][0]++; //  + 9 +
+                            }                                       //  + + +
                         }
                     }
                 }
             }
+        } else { // If not a valid spot for a mine make another mine
+            mines--;
         }
     }
 
-    cnr1 = orig;
-    cnr2 = corner;
-    cnr1.y++;
-    cnr2.y++;
+    // Win condition, used to track if all squares that aren't mines are cleared
+    int clears = X_SIZE * Z_SIZE - MINE_COUNT;
 
-    bool gameRunning = true;
+    mcpp::Coordinate printer = origin;
+    mcpp::Chunk choices = mc.getBlocks(cornerOrigin, cornerOpposite);
+    bool playing = true;
 
-    while (gameRunning) {
-        mcpp::Chunk game = mc.getBlocks(cnr1, cnr2);
-        for (int x = 0; x < size_x; x++) {
-            for (int z = 0; z < size_z; z++) {
+    while (playing) {
+        choices = mc.getBlocks(cornerOrigin, cornerOpposite); // Get whole board
+        for (int x = 0; x < X_SIZE; x++) {
+            for (int z = 0; z < Z_SIZE; z++) { // For all locations in the board
+                printer.x = origin.x + x;
+                printer.z = origin.z + z;
+                printer.y = origin.y; // Set printer pos
 
-                if (game.get(x, 0, z) == mcpp::Blocks::TNT) {
-                    tpos.x = orig.x + x;
-                    tpos.y = orig.y;
-                    tpos.z = orig.z + z;
-                    tpos.y++;
-                    mc.setBlock(tpos, mcpp::Blocks::AIR);
-                    tpos.y--;
+                if (choices.get(x, 0, z) == clear) {
+                    if (board[x][z][1] ==
+                        0) { // If selected to clear and not already cleared
 
-                    if (state[x][z] == 0) {
-                        clearsToWin--;
-                        state[x][z] = 1;
-                        mc.setBlock(tpos, minecols.at(board[x][z]));
+                        mc.setBlock(printer, blocks[board[x][z][0]]); // clear
 
-                        if (board[x][z] == 9) {
-                            mc.postToChat("GameOver");
-                            gameRunning = false;
-                        } else if (board[x][z] == 0) {
-                            std::vector<mcpp::Coordinate> zc;
-                            zc.push_back(tpos);
-                            mc.postToChat("Zero Clear");
-
-                            for (size_t pos = 0; pos < zc.size(); pos++) {
-                                mcpp::Coordinate cord = zc.at(pos);
-                                for (int xmod = -1; xmod <= 1; xmod++) {
-                                    for (int zmod = -1; zmod <= 1; zmod++) {
-                                        if (cord.x + xmod >= orig.x &&
-                                            cord.x + xmod <= corner.x &&
-                                            cord.z + zmod >= orig.z &&
-                                            cord.z + zmod <= corner.z) {
-                                            tpos = cord;
-                                            tpos.x += xmod;
-                                            tpos.z += zmod;
-
-                                            if (state[tpos.x - orig.x]
-                                                     [tpos.z - orig.z] != 1) {
-                                                clearsToWin--;
-                                                if (board[tpos.x - orig.x]
-                                                         [tpos.z - orig.z] ==
-                                                    0) {
-                                                    zc.push_back(tpos);
+                        if (board[x][z][0] == 9) { // If a mine, you loose
+                            mc.postToChat("GAMEOVER!");
+                            playing = false;
+                        } else if (board[x][z][0] ==
+                                   0) { // If zero add location to a stack
+                            std::vector<int*> stack = {new int[2]{x, z}};
+                            unsigned int pos = 0;
+                            while (
+                                pos <
+                                stack.size()) { // until all values in the stack
+                                                // are checked keep stepping
+                                int* back = stack.at(pos);
+                                int zeroX = back[0]; // Reveals & Checks all
+                                                     // values around a 0
+                                int zeroZ = back[1]; // # # #
+                                                     // # x #
+                                for (int xo = -1; xo <= 1; xo++) { // # # #
+                                    if (zeroX + xo >= 0 &&
+                                        zeroX + xo < X_SIZE) {
+                                        for (int zo = -1; zo <= 1; zo++) {
+                                            if (zeroZ + zo >= 0 &&
+                                                zeroZ + zo < Z_SIZE) {
+                                                if (board[zeroX + xo]
+                                                         [zeroZ + zo][0] == 0 &&
+                                                    board[zeroX + xo]
+                                                         [zeroZ + zo][1] != 2) {
+                                                    // if 0 and not already
+                                                    // cleared add to the stack
+                                                    stack.push_back(
+                                                        new int[2]{zeroX + xo,
+                                                                   zeroZ + zo});
                                                 }
+                                                // No spots around a 0 are a
+                                                // mine so clear them all
+                                                printer.x =
+                                                    origin.x + zeroX + xo;
+                                                printer.z =
+                                                    origin.z + zeroZ + zo;
+                                                mc.setBlock(
+                                                    printer,
+                                                    blocks[board[zeroX + xo]
+                                                                [zeroZ + zo]
+                                                                [0]]);
+
+                                                // If not already cleared step
+                                                // towards everything being
+                                                // cleared
+                                                if (board[zeroX + xo]
+                                                         [zeroZ + zo][1] != 2)
+                                                    clears--;
+                                                board[zeroX + xo][zeroZ +
+                                                                  zo][1] =
+                                                    2; // Sets spot to cleared
                                             }
-                                            mc.setBlock(
-                                                tpos,
-                                                minecols.at(
-                                                    board[tpos.x - orig.x]
-                                                         [tpos.z - orig.z]));
-                                            state[tpos.x - orig.x]
-                                                 [tpos.z - orig.z] = 1;
                                         }
                                     }
                                 }
+
+                                pos++;
                             }
                         }
-                    }
-                } else if (game.get(x, 0, z) == mcpp::Blocks::SEA_LANTERN) {
-                    tpos.x = orig.x + x;
-                    tpos.y = orig.y;
-                    tpos.z = orig.z + z;
-
-                    if (state[x][z] == 0) {
-                        state[x][z] = 2;
-                        mc.setBlock(tpos, mcpp::Blocks::SEA_LANTERN);
-                        if (board[x][z] == 9)
-                            flagToWin--;
-                        else
-                            flagToWin++;
-                    } else if (state[x][z] == 2) {
-                        state[x][z] = 0;
-                        mc.setBlock(tpos, mcpp::Blocks::LIGHT_GRAY_CONCRETE);
-                        if (board[x][z] == 9)
-                            flagToWin++;
-                        else
-                            flagToWin--;
+                        if (board[x][z][1] != 2)
+                            clears--;
+                        board[x][z][1] = 2;
                     }
 
-                    tpos.y++;
-                    mc.setBlock(tpos, mcpp::Blocks::AIR);
-                } else if (game.get(x, 0, z) == mcpp::Blocks::BARRIER) {
-                    tpos.x = orig.x + x;
-                    tpos.y = orig.y;
-                    tpos.z = orig.z + z;
-                    tpos.y++;
-                    mc.setBlock(tpos, mcpp::Blocks::AIR);
-                    gameRunning = false;
+                    printer.y++; // removes block above the board
+                    mc.setBlock(printer, air);
+
+                } else if (choices.get(x, 0, z) == flag) { // if set to flag
+                    if (board[x][z][1] == 0) {             // If not a flag
+                        board[x][z][1] = 1;                // make it a flag
+                        mc.setBlock(printer, flag);
+                    } else if (board[x][z][1] == 1) { // if a flag
+                        board[x][z][1] = 0;           // make it not a flag
+                        mc.setBlock(printer, uncleared);
+                    }
+
+                    printer.y++;
+                    mc.setBlock(printer, air);
+
+                } else if (choices.get(x, 0, z) == quit) {
+                    printer.y++;
+                    mc.setBlock(printer, air);
                     mc.postToChat("Quitting");
+                    playing = false;
                 }
             }
         }
 
-        if (clearsToWin == 0 || flagToWin == 0) {
-            mc.postToChat(std::to_string(flagToWin));
-            mc.postToChat(std::to_string(clearsToWin));
-            mc.postToChat("You Win!!");
-            gameRunning = false;
+        if (clears == 0) {
+            mc.postToChat("YOU WIN!!");
+            playing = false;
         }
     }
 
-    for (int x = 0; x < size_x; x++) {
-        for (int z = 0; z < size_z; z++) {
-            tpos = orig;
-            tpos.x += x;
-            tpos.z += z;
-
-            if (state[x][z] == 2) {
-                if (board[x][z] == 9) {
-                    mc.setBlock(tpos, mcpp::Blocks::EMERALD_BLOCK);
+    printer = origin;
+    for (int x = 0; x < X_SIZE; x++) {     // Steps through the whole board
+        for (int z = 0; z < Z_SIZE; z++) { // shows what you did correctly and
+            if (board[x][z][0] == 9) {     // incorrectly
+                if (board[x][z][1] == 1) {
+                    mc.setBlock(printer, goodmine);
+                } else if (board[x][z][1] == 0) {
+                    mc.setBlock(printer, clear);
                 } else {
-                    mc.setBlock(tpos, mcpp::Blocks::GLOWSTONE);
+                    mc.setBlock(printer, badmine);
                 }
-            } else if (board[x][z] == 9) {
-                mc.setBlock(tpos, mcpp::Blocks::TNT);
+            } else if (board[x][z][1] == 1) {
+                mc.setBlock(printer, badflag);
             }
+            printer.z++;
         }
+        printer.z = origin.z;
+        printer.x++;
     }
 }
