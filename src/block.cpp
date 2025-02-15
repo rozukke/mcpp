d437dd93a4f90b9b23d70d815ace08c1e8fe30f1
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2025-02-15 16:52:24 +1100

    chore: apply styling and lint

--- src/block.cpp
+++ src/block.cpp
@@ -2,19 +2,16 @@
 
 namespace mcpp {
 bool BlockType::operator==(const BlockType& other) const {
-    return this->id == other.id && this->mod == other.mod;
+  return this->id == other.id && this->mod == other.mod;
 }
 
-bool BlockType::operator!=(const BlockType& other) const {
-    return !(*this == other);
-}
+bool BlockType::operator!=(const BlockType& other) const { return !(*this == other); }
 
-BlockType BlockType::withMod(int modifier) const {
-    return {this->id, modifier};
-}
+BlockType BlockType::with_mod(uint8_t modifier) const { return {this->id, modifier}; }
 
 std::ostream& operator<<(std::ostream& out, const BlockType& block) {
-    out << "[" << block.id << ", " << block.mod << "]";
-    return out;
+  using std::to_string;
+  out << "[" << to_string(block.id) << ", " << to_string(block.mod) << "]";
+  return out;
 }
 } // namespace mcpp
