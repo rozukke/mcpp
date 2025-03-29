a8284af40783bc5058d797c32fd1dc23e53c2b62
darcy <acednes@gmail.com>
2025-03-29 17:13:21 +1100

    fix: make `Coordinate2D::withHeight` const

--- include/mcpp/util.h
+++ include/mcpp/util.h
@@ -142,7 +142,7 @@ struct Coordinate2D {
      * @param coord The Coordinate2D object.
      * @param y The y value.
      */
-    constexpr Coordinate withHeight(int y);
+    constexpr Coordinate withHeight(int y) const;
 
     /**
      * @brief Adds two Coordinate2D objects.
@@ -201,7 +201,7 @@ struct Coordinate2D {
     int z;
 };
 
-constexpr Coordinate Coordinate2D::withHeight(int y) {
+constexpr Coordinate Coordinate2D::withHeight(int y) const {
     return Coordinate(this->x, y, this->z);
 }
 
